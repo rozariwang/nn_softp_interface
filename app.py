358a22b4b843fffa0f432790d521bb02f5959de1
hohsuanwang <134708973+rozariwang@users.noreply.github.com>
2024-03-16 20:58:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from datetime import datetime
 import random_generator
 import subprocess
 import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 if 'current_page' not in st.session_state:
     st.session_state['current_page'] = "Main Page"
@@ -14,18 +16,35 @@ if 'current_page' not in st.session_state:
 def change_page(page_name):
     st.session_state['current_page'] = page_name
 
+# Function to initialize Google Sheets API connection
+def init_google_sheets_connection():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/google-credentials.json", scope)
+    client = gspread.authorize(creds)
+    return client
+
+# Function to append data to the specified Google Sheet
+def append_data_to_google_sheet(data, sheet_name, worksheet_index=0):
+    client = init_google_sheets_connection()
+    sheet = client.open(sheet_name).get_worksheet(worksheet_index)
+    # Convert the data dictionary to a list of values and append to the Google Sheet
+    values_list = list(data.values())
+    sheet.append_row(values_list)
+
+
 # Page navigation buttons
-st.sidebar.title("Navigation")
+st.sidebar.title("🚀 Navigation")
 st.sidebar.button("Main Page", on_click=change_page, args=("Main Page",))
 st.sidebar.button("Fact-Checking Links", on_click=change_page, args=("Fact-Checking Links",))
-st.sidebar.button("Dataset", on_click=change_page, args=("Dataset",))
+st.sidebar.button("Dataset", on_click=change_page, args=("Datasets",))
 st.sidebar.button("Model Structure", on_click=change_page, args=("Model Structure",))
 
 #########################
 #### Main Page       ####
 #########################
 if st.session_state['current_page'] == "Main Page":
-    st.title("News Classifier with Surprisal Values")
+    st.title("🚩 News Classifier 🚩")
 
     # Text input for the article title and body
     article_title = st.text_area("Article Title:", "Enter the article title here...")
@@ -37,15 +56,18 @@ if st.session_state['current_page'] == "Main Page":
     # Numeric input for threshold
     threshold = st.number_input("Set the threshold for classification:", min_value=0.0, max_value=1.0, value=0.5)
 
-    # Initialize session state for classification agreement and reason for disagreement
-    if 'classification_agreement' not in st.session_state:
-        st.session_state['classification_agreement'] = None
-    if 'reason_for_disagreement' not in st.session_state:
-        st.session_state['reason_for_disagreement'] = ''
-    
+    classification_agreement = st.radio(
+        "Do you agree with the classification?",
+        ["Yes", "No"],
+    )
+
+    # Feedback box
+    reason_for_disagreement = st.text_area("Please provide your reason for disagreement:")
+
     if st.button("Classify"):
         classification, surprisal_values, words = random_generator.generate_surprisal_values(article_body, threshold)
         st.write(f"Classification: {classification}")
+
 
         # Normalize surprisal values for color mapping
         normalized_vals = np.interp(surprisal_values, (min(surprisal_values), max(surprisal_values)), (0, 1))
@@ -70,64 +92,26 @@ if st.session_state['current_page'] == "Main Page":
         # Display the custom heatmap in Streamlit
         st.markdown(html_content, unsafe_allow_html=True)
 
-        # Define a key for the select box to easily access its value from st.session_state
-        classification_agreement = st.radio(
-            "Do you agree with the classification?",
-            ["Yes", "No"],
-        )
-
-        # A button to confirm the choice
-        if st.button('Confirm'):
-            # Use session_state to remember the user's choice even after rerun
-            st.session_state['confirmed_agreement'] = classification_agreement
-
-            # Once confirmed, display the feedback box if they disagreed
-            if classification_agreement == "No":
-                st.session_state['show_feedback_box'] = True
-            else:
-                st.session_state['show_feedback_box'] = False
-                # Optionally, handle the "Yes" case or reset state as necessary
-
-        # Conditional display based on session_state, to persist across reruns
-        if st.session_state.get('show_feedback_box', False):
-            reason_for_disagreement = st.text_area("Please provide your reason for disagreement:")
-            st.session_state['reason_for_disagreement'] = reason_for_disagreement
-   
-            # Saving data to CSV
-            data = {
-                'date': [datetime.now()],
-                'title': [article_title],
-                'body': [article_body],
-                'link': [source_link],
-                'classification_result': [classification],
-                'user_agreement': [classification_agreement],
-                'disagreement_reason': [reason_for_disagreement if classification_agreement == "No" else ''],
-            }
-            df = pd.DataFrame(data)
-            # Append the data to 'data.csv', creating if doesn't exist
-            df.to_csv('data.csv', mode='a', header=not pd.read_csv('data.csv').empty, index=False)
-
-    
-    repo_path = '/the/correct/full/path/to/your/git/repository'  # Update this
 
     if st.button("Save"):
+        data = {
+            'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'title': article_title,
+            'body': article_body,
+            'link': source_link,
+            'classification_result': classification,
+            'user_agreement': classification_agreement,
+            'disagreement_reason': reason_for_disagreement if classification_agreement == "No" else '',
+        }
+    
+        # Specify your Google Sheet name here
+        sheet_name = "Your Google Sheet Name"
+    
         try:
-            # Change to the repository directory
-            os.chdir(repo_path)
-        
-            # Attempt to add, commit, and push changes
-            subprocess.run(["git", "add", "data.csv"], check=True)
-            commit_result = subprocess.run(["git", "commit", "-m", "Update data.csv"], capture_output=True, text=True)
-        
-            # Only attempt to push if the commit was successful
-            if commit_result.returncode == 0:
-                push_result = subprocess.run(["git", "push"], check=True, capture_output=True, text=True)
-                st.success("Changes saved and pushed to GitHub successfully!")
-            else:
-                st.error(f"Failed to commit changes. {commit_result.stderr}")
-            
-        except subprocess.CalledProcessError as e:
-            st.error(f"An error occurred: {e}")
+            append_data_to_google_sheet(data, sheet_name)
+            st.success("Data saved to Google Sheet successfully!")
+        except Exception as e:
+            st.error(f"Failed to save data to Google Sheet: {e}")
         
 
 #########################
